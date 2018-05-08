5f9514330cd6f4aa6fbac6ccc1cb3ee5237a5048
Guillaume Le Cousin <guillaume@lecousin.net>
2018-05-08 06:27:06 +0200

    IconProvider: add functions to get all sizes and icons

--- src/main/java/net/lecousin/framework/uidescription/resources/IconProvider.java
+++ src/main/java/net/lecousin/framework/uidescription/resources/IconProvider.java
@@ -1,5 +1,7 @@
 package net.lecousin.framework.uidescription.resources;
 
+import java.util.Arrays;
+
 import net.lecousin.framework.geometry.HorizontalAlignment;
 import net.lecousin.framework.geometry.VerticalAlignment;
 
@@ -8,6 +10,12 @@ public interface IconProvider {
 
 	/** Provides the icon for a given size. */
 	public ImageDescriptor getIcon(int size);
+	
+	/** Provides all available icons. */
+	public ImageDescriptor[] getIcons();
+	
+	/** Provides all available sizes. */
+	public int[] getSizes();
 	
 	/** Provides icons from a predefined path. */
 	public static class FromPath implements IconProvider {
@@ -32,6 +40,18 @@ public interface IconProvider {
 			return ResourcesManager.getImage(prefix + s + suffix);
 		}
 
+		@Override
+		public ImageDescriptor[] getIcons() {
+			ImageDescriptor[] icons = new ImageDescriptor[available.length];
+			for (int i = 0; i < available.length; ++i)
+				icons[i] = ResourcesManager.getImage(prefix + available[i] + suffix);
+			return icons;
+		}
+		
+		@Override
+		public int[] getSizes() {
+			return Arrays.copyOf(available, available.length);
+		}
 	}
 	
 	/** Provides decorated icons. */
@@ -57,6 +77,19 @@ public interface IconProvider {
 			return new ImageDecorated(m, d, valign, halign);
 		}
 		
+		@Override
+		public ImageDescriptor[] getIcons() {
+			int[] sizes = main.getSizes();
+			ImageDescriptor[] icons = new ImageDescriptor[sizes.length];
+			for (int i = 0; i < icons.length; ++i)
+				icons[i] = new ImageDecorated(main.getIcon(sizes[i]), decoration.getIcon(sizes[i] / 2), valign, halign);
+			return icons;
+		}
+		
+		@Override
+		public int[] getSizes() {
+			return main.getSizes();
+		}
 	}
 	
 }
